9d9d873b15a69c029a8cb6303ddf8bf1fdb3d603
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-10-14 23:39:23 +0200

    adding min and max csv line to env

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -1,4 +1,10 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 // returns true if the rows is in limit specified
 export const isWithinLimit = (lineCount: number): boolean => {
-  return lineCount >= 1 && lineCount <= 10000
+  return (
+    lineCount >= Number(process.env.MIN_CSV_LINE_COUNT || 1) &&
+    lineCount <= Number(process.env.MAX_CSV_LINE_COUNT || 10000)
+  )
 }
